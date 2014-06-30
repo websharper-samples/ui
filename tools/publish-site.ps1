a15eada10301d0bd941e5bd26bc9e175013e67a2
András Jankó <andras.janko@gmail.com>
2014-06-30 16:21:54 +0200

    fixing paths in publish-site.ps1

--- tools/publish-site.ps1
+++ tools/publish-site.ps1
@@ -2,10 +2,10 @@
 rm -r build -errorAction ignore
 $d = mkdir build
 $d = mkdir build/html
-cp -r Site/Content build/html/
-cp -r Site/*.jpg build/html/
-cp -r Site/*.css build/html/
-cp -r Site/*.html build/html/
-cp -r Site/*.json build/html/
+cp -r src/Content build/html/
+cp -r src/*.jpg build/html/
+cp -r src/*.css build/html/
+cp -r src/*.html build/html/
+cp -r src/*.json build/html/
 
 
