98811b1b52dbe7654e37e3d7a114a3c5e75dedb7
Simon Fowler <simon@simonjf.com>
2014-07-29 16:23:40 +0200

    Add SiteCommon.fs

---
+++ src/SiteCommon.fs
@@ -0,0 +1,47 @@
+﻿namespace IntelliFactory.WebSharper.UI.Next
+
+open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.UI.Next
+open IntelliFactory.WebSharper.UI.Next.Html
+
+[<AutoOpen>]
+[<JavaScript>]
+module SiteCommon =
+    type PageTy = | Home | About | Samples
+
+    type Meta =
+        {
+            FileName : string
+            Keywords : list<string>
+            Title : string
+            Uri : string
+        }
+
+    type Sample =
+        {
+            mutable Body : Doc
+            mutable Description : Doc
+            Meta : Meta
+            mutable Router : Router<Page>
+            mutable RouteId : RouteId
+            mutable SamplePage : Page
+        }
+    and Page =
+        {
+            mutable PageName : string
+            mutable PageRouteId : RouteId
+            //mutable PageRender : Doc
+            mutable PageType : PageTy
+            mutable PageSample : Sample option
+            //mutable PageRenderVar : Var<PageTy>
+        }
+
+    let mkPage name routeId ty = //render ty rv =
+        {
+            PageName = name
+            PageRouteId = routeId
+           // PageRender = render
+            PageType = ty
+            PageSample = None
+            //PageRenderVar = rv
+        }
