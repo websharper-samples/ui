db580308185afa92bbfa8940563a7303dbd3f173
F.D.Castel <fdcastel@gmail.com>
2015-10-30 07:08:01 -0200

    Removed copyright notice ("This file is confidential and proprietary"?) from .fs files.

--- src/Client.fs
+++ src/Client.fs
@@ -1,14 +1,3 @@
-// $begin{copyright}
-//
-// This file is confidential and proprietary.
-//
-// Copyright (c) IntelliFactory, 2004-2014.
-//
-// All rights reserved.  Reproduction or use in whole or in part is
-// prohibited without the written consent of the copyright holder.
-//-----------------------------------------------------------------
-// $end{copyright}
-
 namespace WebSharper.UI.Next
 
 open WebSharper
--- src/PhoneExample.fs
+++ src/PhoneExample.fs
@@ -1,15 +1,4 @@
-﻿// $begin{copyright}
-//
-// This file is confidential and proprietary.
-//
-// Copyright (c) IntelliFactory, 2004-2014.
-//
-// All rights reserved.  Reproduction or use in whole or in part is
-// prohibited without the written consent of the copyright holder.
-//-----------------------------------------------------------------
-// $end{copyright}
-
-namespace WebSharper.UI.Next
+﻿namespace WebSharper.UI.Next
 
 open WebSharper
 open WebSharper.UI.Next
